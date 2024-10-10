e984f9368c9ad8e8535bbf51f3f4636f68aa1b1c
4028-controls <4028.controls@gmail.com>
2024-10-10 19:05:04 -0400

    Fixed this dir

--- 4028-Template-Lesson2-Starting-Code/src/main/java/frc/robot/Robot.java
+++ 4028-Template-Lesson2-Starting-Code/src/main/java/frc/robot/Robot.java
@@ -4,95 +4,139 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkRelativeEncoder.Type;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
- * 🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪 🟪🟪🟪🟪
- * TASK: Create a motor and spin it a fixed number of rotataions 🟪🟪🟪🟪
- * 🟪🟪🟪🟪 The motor should spin +6 rotations on a Y press and -5 🟪🟪🟪🟪
- * 🟪🟪🟪🟪 rotations on an X press. Finally, if the motor is at 🟪🟪🟪🟪
- * 🟪🟪🟪🟪 the target position, have it run forward at 0.1 vbus 🟪🟪🟪🟪
- * 🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪🟪
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-	private Command m_autonomousCommand;
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> chooser = new SendableChooser<>();
 
-	// 🟪🟪🟪🟪 Declare a new CANSparkMax motor here 🟪🟪🟪🟪
+  private CANSparkMax NicksEpicestMotor;
+  private CommandXboxController NicksBoringControler;
+  private RelativeEncoder Nicolisuperdupperencoderorder;
 
-	private RobotContainer m_robotContainer;
-	private CommandXboxController driverController = new CommandXboxController(0);
-	private static final int CAN_ID = 13;
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", chooser);
 
-	// 🟪🟪🟪🟪 Declare your targetPosition and constants here (ROTATIONS_TO_SPIN,
-	// TOELRANCE, and CLOSED_LOOP_VBUS) 🟪🟪🟪🟪
+    NicksEpicestMotor = new CANSparkMax(13, MotorType.kBrushed);
+    NicksBoringControler = new CommandXboxController(0);
+    Nicolisuperdupperencoderorder = NicksEpicestMotor.getEncoder(Type.kQuadrature, 4096);
 
-	@Override
-	public void robotInit() {
-		m_robotContainer = new RobotContainer();
+  }
 
-		// 🟪🟪🟪🟪 Initialize your CANSparkMax here (use the CAN_ID constant)...
-		// 🟪🟪🟪🟪
-	}
-	@Override
-	public void robotPeriodic() {
-		CommandScheduler.getInstance().run();
-	}
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
 
-	@Override
-	public void disabledInit() {
-	}
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
 
-	@Override
-	public void disabledPeriodic() {
-	}
-	@Override
-	public void autonomousInit() {
-		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
 
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.schedule();
-		}
-	}
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
 
-	@Override
-	public void autonomousPeriodic() {
-	}
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    if (NicksBoringControler.getLeftY() < -0.2) {
+        if (Nicolisuperdupperencoderorder.getPosition() >= -30.0) {
+            NicksEpicestMotor.set(NicksBoringControler.getLeftY());
+        }
+        else {
+            NicksEpicestMotor.set(0);
+        }
+    }
+    else if (NicksBoringControler.getLeftY() > 0.2) {
+        if(Nicolisuperdupperencoderorder.getPosition() <= 30.0) {
+            NicksEpicestMotor.set(NicksBoringControler.getLeftY());
+        }
+        else {
+            NicksEpicestMotor.set(0);
+        }
+    }
+    else {
+        NicksEpicestMotor.set(0);
+    }
+    SmartDashboard.putNumber("Motor Revolutions", Nicolisuperdupperencoderorder.getPosition());
+  }
 
-	@Override
-	public void teleopInit() {
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
-		}
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
 
-		// 🟪🟪🟪🟪 Bind your buttons here... 🟪🟪🟪🟪
-	}
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
 
-	@Override
-	public void teleopPeriodic() {
-		// 🟪🟪🟪🟪 Here is where you'll set the motor control 🟪🟪🟪🟪
-		// 🟪🟪🟪🟪 Remember, move it toward the target if 🟪🟪🟪🟪
-		// 🟪🟪🟪🟪 it's farther away from the targetPosition 🟪🟪🟪🟪
-		// 🟪🟪🟪🟪 than the threshold, otherwise run it at 🟪🟪🟪🟪
-		// 🟪🟪🟪🟪 10% vbus (0.1) 🟪🟪🟪🟪
-	}
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
 
-	@Override
-	public void testInit() {
-		CommandScheduler.getInstance().cancelAll();
-	}
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 
-	@Override
-	public void testPeriodic() {
-	}
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
 
-	@Override
-	public void simulationInit() {
-	}
-
-	@Override
-	public void simulationPeriodic() {
-	}
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
